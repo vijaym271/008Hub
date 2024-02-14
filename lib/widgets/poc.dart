f0ce64da50ae8a6cfaa7f6df47db09e2385a09cf
vijay <vijaym000111@gmail.com>
2024-02-14 16:13:53 +0530

    poc_added

---
+++ lib/widgets/poc.dart
@@ -0,0 +1,116 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('Draggable Widget Example'),
+//         ),
+//         body: DraggableWidget(),
+//       ),
+//     );
+//   }
+// }
+
+// class DraggableWidget extends StatefulWidget {
+//   @override
+//   _DraggableWidgetState createState() => _DraggableWidgetState();
+// }
+
+// class _DraggableWidgetState extends State<DraggableWidget> {
+//   Offset position = Offset(0.0, 0.0);
+//   double imgWidth = 0.0;
+//   double imgHeight = 0.0;
+//   double txtWidth = 0.2;
+//   double txtHeight = 0.2;
+//   GlobalKey _imgKey = GlobalKey();
+
+//   void getWidgetDimensions() {
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       RenderBox renderBox =
+//           _imgKey.currentContext!.findRenderObject() as RenderBox;
+//       Future.delayed(const Duration(milliseconds: 100), () {
+//         setState(() {
+//           imgWidth = renderBox.size.width.roundToDouble();
+//           imgHeight = renderBox.size.height.roundToDouble();
+//         });
+//       });
+//     });
+//   }
+
+//   double convertToPt(double input, double maxValue) {
+//     if (input == 0.0) return input;
+//     double minValue = 0.0;
+//     double result = (input - minValue) / (maxValue - minValue);
+//     return double.parse(result.toStringAsFixed(3));
+//   }
+
+//   double convertToPr(double input, double maxValue) {
+//     double minValue = 0.0;
+//     double result = (input * (maxValue - minValue)) + minValue;
+//     return double.parse(result.toStringAsFixed(3));
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final screenWidth = MediaQuery.of(context).size.width;
+//     final screenHeight = MediaQuery.of(context).size.height;
+
+//     print('i am position -->$position');
+//     print('i am txt -->${txtWidth} ${txtHeight}');
+// //     print('i am img -->$imgWidth $imgHeight');
+// //     print('i am txt -->${txtWidth * imgWidth} ${txtHeight * imgHeight}');
+// //     print('i am device -->$screenWidth $screenHeight');
+// //     print('i am top -->${mapValue(position.dx, imgWidth)}');
+// //     print('i am left -->${mapValue(position.dx, imgWidth)}');
+
+//     return Stack(
+//       children: <Widget>[
+//         Container(
+//           key: _imgKey,
+//           width: 0.7 * screenWidth,
+//           height: 0.7 * screenHeight,
+//           color: Colors.lightGreen,
+//           child: LayoutBuilder(builder: (context, constraint) {
+//             if (constraint.maxWidth.roundToDouble() != imgWidth) {
+//               getWidgetDimensions();
+//             }
+//             return SizedBox.shrink();
+//           }),
+//         ),
+//         Positioned(
+//           left: position.dx * imgWidth,
+//           top: position.dy * imgHeight,
+//           child: GestureDetector(
+//             child: Container(
+//               width: txtWidth * imgWidth,
+//               height: txtHeight * imgHeight,
+//               color: Colors.blue,
+//               child: Center(
+//                 child: Text('Drag'),
+//               ),
+//             ),
+//             onPanUpdate: (offset) {
+//               setState(() {
+//                 double x =
+//                     (offset.delta.dx + convertToPr(position.dx, imgWidth))
+//                         .clamp(0.0, imgWidth - (txtWidth * imgWidth));
+//                 double y =
+//                     (offset.delta.dy + convertToPr(position.dy, imgHeight))
+//                         .clamp(0.0, imgHeight - (txtHeight * imgHeight));
+//                 position =
+//                     Offset(convertToPt(x, imgWidth), convertToPt(y, imgHeight));
+//               });
+//             },
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
